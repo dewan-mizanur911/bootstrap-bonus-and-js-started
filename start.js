5f62a63f0e57c5237e5f774c3de9978979de4db4
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-07-30 12:30:37 +0600

    collage file and js file added

---
+++ start.js
@@ -0,0 +1,4 @@
+console.log('This Is Dewan Mizanur Rahman');
+console.log(143);
+console.log('various element to show up the process.We are learning javascript.');
+console.log('Thank you very much Mr.360');
